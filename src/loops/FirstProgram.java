eb914d212ccf51763af2a6d4c94f7946d18a5fb4
lalitbarpete1 <lalit.barpete@gmail.com>
2026-01-08 19:58:59 +0530

    added some file

--- src/loops/FirstProgram.java
+++ src/loops/FirstProgram.java
@@ -4,5 +4,10 @@ public class FirstProgram {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
+		int n=0;
+		for (n=0;n<=20;n=n+2) 
+		{
+			System.out.println(n);
+		}
 	}
 }
--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,5 +1,10 @@
 package pages;
 
 public class HomePage {
+	public static void main(String[] args) {
+		String username ="lalit.barpete";
+		String password = "Waves@805";
+		
+	}
 
 }
