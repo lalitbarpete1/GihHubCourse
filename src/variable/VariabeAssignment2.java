f8dbc5e888dee937e70b23a4208fc2bef0370f74
lalitbarpete1 <lalit.barpete@gmail.com>
2026-01-14 16:37:36 +0530

    added assigement 2

---
+++ src/variable/VariabeAssignment2.java
@@ -0,0 +1,35 @@
+package variable;
+
+public class VariabeAssignment2 {
+
+	int age = 10; // instance variable
+	int mark = 20; // instance variable
+	static int c = 30; // static variable
+	static int d = 40; // static variable
+
+	void m1() {
+		System.out.println(age);
+		System.out.println(mark);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+	static void m2()
+
+	{
+		VariabeAssignment2 obj1 = new VariabeAssignment2();
+
+		System.out.println(obj1.age);
+		System.out.println(obj1.mark);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+	public static void main(String[] args) {
+		VariabeAssignment2 obj2 = new VariabeAssignment2();
+		obj2.m1();
+		m2();
+
+	}
+}
